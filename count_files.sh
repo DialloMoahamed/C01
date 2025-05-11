f085abd108c2a06a2fd132efa8f9adb0b1ab5187
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 02:08:19 +0100

    correction exo

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# ls -p "$1" : liste le contenu du dossier, avec / ajouté à la fin des dossiers
-# grep -v / : enlève les lignes contenant / → on garde seulement les fichiers
-# wc -l : compte le nombre de lignes, donc le nombre de fichiers
-# $(...) : exécute la commande et stocke le résultat dans la variable count
-# ls -1 permet de lister les fichiers dans le répertoire, un par ligne
-# wc -l permet de dénombrer les lignes de la sortie standard
 
-echo "Le dossier $1 contient$(ls -l "$1" | wc -l) fichier(s)."
+echo "Le dossier $1 contient $(find "$1" -maxdepth 1 -type f | wc -l) fichier(s)."
