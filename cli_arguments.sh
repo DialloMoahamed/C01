33d5ef6709191297bd0366fecef26e84f0a789db
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 18:21:41 +0100

    Correction

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-echo "Entrez vos arguments (séparés par des espaces) :"
 read -a arguments
 
 i=1
 for argument in "${arguments[@]}"; do
-  echo -e "Argument $i: $argument"
+  echo "Argument $i: $argument"
   ((i++))
 done
