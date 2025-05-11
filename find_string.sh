83ce9a24eb67602a4fd8b20fc648cee7ced04ca1
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 14:32:05 +0100

    Push

--- find_string.sh
+++ find_string.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+read fichier
+read mot
+
+if grep -q "$mot" "$fichier" ; then
+  echo "La chaîne '$mot' a été trouvée dans $fichier."
+else
+  echo "La chaîne '$mot' n'a pas été trouvée dans $fichier."
+fi
