a8de21eb8352faaeb67edf1513f75aa328901a9b
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 14:14:16 +0100

    push test5

--- count_files.sh
+++ count_files.sh
@@ -2,6 +2,6 @@
 echo "Entrez le nom d'un dossier :"
 read dossier
 
-count=$(ls -p "$dossier" | grep -v / | wc -l)
+count=$(ls -l "$dossier" | wc -l)
 counts=$(echo $count)
 echo "Le dossier $dossier contient $counts fichier(s)."
