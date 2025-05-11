25d7fd2b1911ae5c723b14c4d4516db1aef10c07
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 03:27:57 +0100

    test1

--- cli_arguments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script qui affiche les arguments un par un
-
-i=1
-for arg in "$@"; do
-    echo "Argument $i: $arg"
-    ((i++))
-done
--- count_files.sh
+++ count_files.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-# Script qui compte le nombre de fichiers dans un répertoire
 
-FOLDER="$1"
-
-# Vérifie si le répertoire existe
-if [ ! -d "$FOLDER" ]; then
-    echo "Erreur : '$FOLDER' n'est pas un répertoire valide."
-    exit 1
-fi
-
-# Compte les fichiers (uniquement les fichiers, pas les dossiers)
-FILE_COUNT=$(find "$FOLDER" -type f | wc -l)
-
-echo "Le dossier $FOLDER contient $FILE_COUNT fichier(s)."
