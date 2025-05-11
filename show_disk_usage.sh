1b62f03ccdb101081ff325980ba03e03b26fbb75
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 02:40:56 +0100

    correction exo4

---
+++ show_disk_usage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Ce script permet d'affiche l'utilisation du disque de façon lisible
+# df: Cette commande affiche l'utilisation de l'espace disque.
+# -h: Cette option permet de rendre la sortie lisible par l'homme, en utilisant des unités de mesure plus courantes (Go, Mo, etc).
+
+echo "Utilisation du disque:"
+df -h
