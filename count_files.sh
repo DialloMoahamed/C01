159c6b14746b1c984cf0f5b342a388dffaf81bb4
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 02:22:18 +0100

    test

--- count_files.sh
+++ count_files.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Vérifie qu’un argument est fourni
-if [ -z "$1" ]; then
-    echo "Usage: $0 <nom_du_dossier>"
-    exit 1
-fi
-
 # Vérifie que l'argument est un dossier
 if [ ! -d "$1" ]; then
     echo "Erreur: '$1' n'est pas un dossier."
