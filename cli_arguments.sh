cae3b7224f34065ddb501401e5f9ca6d749d3104
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 18:19:22 +0100

    Correction eox3

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo "Entrez vos arguments (séparés par des espaces) :"
+read -a arguments
+
+i=1
+for argument in "${arguments[@]}"; do
+  echo -e "Argument $i: $argument"
+  ((i++))
+done
