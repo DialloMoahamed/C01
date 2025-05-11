0306ca617fbdd41bab5236c8e92e3d97c31974a0
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 02:38:13 +0100

    Pusher

--- find_string.sh
+++ find_string.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# Script pour rechercher une chaîne de caractères dans un fichier
 
+if grep -q "$2" "$1"; then
+  echo "La chaîne '$2' a été trouvée dans $1."
+else
+  echo "La chaîne '$2' n'a pas été trouvée dans $1."
+fi
