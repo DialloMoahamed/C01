389cf211d88270881df318989503b35e5bb56e0f
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-11 02:30:07 +0100

    valider

--- count_files.sh
+++ count_files.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Vérifie que l'argument est un dossier
-if [ ! -d "$1" ]; then
-    echo "Erreur: '$1' n'est pas un dossier."
-    exit 1
-fi
-
 count=$(find "$1" -maxdepth 1 -type f | wc -l | tr -d ' ')
 echo "Le dossier $1 contient $count fichier(s)."
 
